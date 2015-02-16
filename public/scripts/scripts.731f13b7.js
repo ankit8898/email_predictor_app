f4dc41a2fbc89b146ec314a4c136b8b764de489a
Ankit gupta <ankit.gupta8898@gmail.com>
2015-02-16 09:55:42 -0500

    Builing dist

--- public/scripts/scripts.731f13b7.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("webAppEmailPredictorApp",[]),angular.module("webAppEmailPredictorApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
